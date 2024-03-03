63ec8ba2135599c91364470078772c8540a531e3
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-03-03 02:51:06 -0500

    Added a second route and tested it

--- express_server.js
+++ express_server.js
@@ -30,3 +30,11 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  const templateVars = { id: id, longURL: longURL };
+  res.render("urls_show", templateVars);
+  
+});
